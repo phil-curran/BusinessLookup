9aebc763de21cdb2f9e83f562fb7679566d4c10e
Phil Curran <pecurran@hotmail.com>
2022-04-23 09:36:32 -0700

    moved .git folder to parent folder before creating BusinessClient.Solution.

--- BusinessLookup.Solution/Models/Business.cs
+++ BusinessLookup.Solution/Models/Business.cs
@@ -1,20 +1,32 @@
-using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusinessLookup.Models
 {
+  [Table("Business")]
   public class Business
   {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Address { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string State { get; set; }
+    [Required]
     public int Zip { get; set; }
+    [Required]
     public string Phone { get; set; }
+    [Required]
     public string Email { get; set; }
     public string Web { get; set; }
     public string Price { get; set; }
+    [Required]
     public string Category { get; set; }
+    [Required]
     public string Subcategory { get; set; }
   }
 }
